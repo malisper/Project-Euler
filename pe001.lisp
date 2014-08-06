7f9a04b313df9521847bb0895a6ad8da1a0af2bf
Michael <michaelmalis2@gmail.com>
2014-08-06 00:26:33 -0500

    Cleaned up

--- pe001.lisp
+++ pe001.lisp
@@ -1,7 +1,9 @@
 (in-package :clamp)
 
+(defparameter largest* 1000)
+
 (def solve ()
-  (loop for i from 1 below 1000
+  (loop for i from 1 below largest*
         if (or (multiple i 3)
 	       (multiple i 5))
         sum i))
