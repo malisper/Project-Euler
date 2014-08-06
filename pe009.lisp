04eea3867039c39168fc61de8a251ef6c2bad03a
Michael <michaelmalis2@gmail.com>
2014-08-06 00:53:24 -0500

    Added Documentation

--- pe009.lisp
+++ pe009.lisp
@@ -1,9 +1,10 @@
 (in-package :clamp)
 
-(defparameter total* 1000)
+(defparameter total* 1000
+  "What the sum of the three numbers has to be.")
 
 (def sqr (n)
-  "The square of n."
+  "Returns the square of its argument."
   (* n n))
 
 (def triple (a b c)
@@ -12,7 +13,7 @@
       (sqr c)))
 
 (def solve ()
-  "Solve PE problem 9"
+  "Solve PE problem 9."
   (upto a 1 total*
     (upto b (+ a 1) total*
       (let c (- total* a b)
