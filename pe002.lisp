50ca86e945dd4ec2bb9db96cfce133b4fac79c17
Michael <michaelmalis2@gmail.com>
2014-08-06 00:46:37 -0500

    Added Documentation

--- pe002.lisp
+++ pe002.lisp
@@ -1,8 +1,10 @@
 (in-package :clamp)
 
-(defparameter largest* 4000000)
+(defparameter largest* 4000000
+  "The upper bound on the problem.")
 
 (def solve ()
+  "Solve PE problem 2."
   (loop for n   = 1 then (+ n n-1)
         and n-1 = 0 then n
         while (< n largest*)
