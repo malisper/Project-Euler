7283d06e7194fb65770bca1c3c2cb2d64cfd5674
Michael <michaelmalis2@gmail.com>
2014-08-06 00:45:47 -0500

    Added Documentation

--- pe001.lisp
+++ pe001.lisp
@@ -1,8 +1,10 @@
 (in-package :clamp)
 
-(defparameter largest* 1000)
+(defparameter largest* 1000
+  "The upper bound on the problem")
 
 (def solve ()
+  "Solve PE problem 1"
   (loop for i from 1 below largest*
         if (or (multiple i 3)
 	       (multiple i 5))
