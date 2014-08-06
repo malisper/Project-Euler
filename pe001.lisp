e6207fbe92a16e4c0e8ebd1290c52314389cdf72
Michael <michaelmalis2@gmail.com>
2014-08-06 00:46:10 -0500

    punctuation

--- pe001.lisp
+++ pe001.lisp
@@ -1,10 +1,10 @@
 (in-package :clamp)
 
 (defparameter largest* 1000
-  "The upper bound on the problem")
+  "The upper bound on the problem.")
 
 (def solve ()
-  "Solve PE problem 1"
+  "Solve PE problem 1."
   (loop for i from 1 below largest*
         if (or (multiple i 3)
 	       (multiple i 5))
