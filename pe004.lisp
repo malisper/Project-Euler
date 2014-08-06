3f7578231db11390b7df26e0ed5076b3768e9448
Michael <michaelmalis2@gmail.com>
2014-08-06 00:47:49 -0500

    Added Documentation

--- pe004.lisp
+++ pe004.lisp
@@ -1,13 +1,15 @@
 (in-package :clamp)
 
-(defparameter lowest* 100)
-(defparameter largest* 1000)
+(defparameter lowest* 100 "The lower bound on the problem.")
+(defparameter largest* 1000 "The upper bound on the problem.")
 
 (def palindrome (n)
+  "Is this number a palindrome?"
   (ado (tostring (pr n))
        (iso it (rev it))))
 
 (def solve ()
+  "Solve PE problem 4."
   (ret result 0
     (up n1 lowest* largest*
       (up n2 n1 largest*
