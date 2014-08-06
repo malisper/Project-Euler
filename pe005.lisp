1ccf5837ca261196d56b22382463671d1182acb1
Michael <michaelmalis2@gmail.com>
2014-08-06 00:48:32 -0500

    Added Documentation

--- pe005.lisp
+++ pe005.lisp
@@ -1,6 +1,8 @@
 (in-package :clamp)
 
-(defparameter largest* 20)
+(defparameter largest* 20
+  "The upper bound on the problem.")
 
 (def solve ()
+  "Solve PE problem 5"
   (reduce #'lcm (range 1 largest*)))
