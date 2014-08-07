e42c3b1852b5ff041b0185c2acbc4cc61d44fd13
Michael <michaelmalis2@gmail.com>
2014-08-06 22:39:18 -0500

    Solved Problem 48

---
+++ pe048.lisp
@@ -0,0 +1,19 @@
+(in-package :clamp)
+
+(defparameter largest* 1000
+  "The upper bound on the problem.")
+
+(def self-power (n)
+  "Raise n to the nth power."
+  (expt n n))
+
+(def last-digits (d n)
+  "Gets the last d digits of n."
+  (mod n (expt 10 d)))
+
+(def solve ()
+  "Solves PE problem 48."
+  (last-digits 10
+	       (loop for i from 1 to largest*
+		     sum (self-power i))))
+
