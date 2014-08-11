78c936e516845ab2a6ad8e9c05659d2950f75e22
Michael <michaelmalis2@gmail.com>
2014-08-11 14:42:42 -0500

    Refactored The Code

--- pe007.lisp
+++ pe007.lisp
@@ -3,6 +3,9 @@
 ;; https://github.com/nakrakiiya/cl-prime-maker
 (use-package :cl-prime-maker)
 
+(defparameter number* 10001
+  "The number we need for the problem.")
+
 (def solve ()
   "Solves PE problem 7."
-  (get-nth-prime 10001))
+  (get-nth-prime number*))
