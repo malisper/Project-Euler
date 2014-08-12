7dc73b0af106ff5b5f32c4d0f9cc9e30c9755201
Michael <michaelmalis2@gmail.com>
2014-08-12 18:29:20 -0500

    fixed indentation

--- pe039.lisp
+++ pe039.lisp
@@ -1,25 +1,25 @@
 (in-package :clamp)
 
 (def sqr (x)
-	"Returns the square of x."
-	(* x x))
+  "Returns the square of x."
+  (* x x))
 
 (def triple (a b c)
-	"Are a, b, and c a pythagorean triple?"
-	(and (< a b c)
-			 (is (+ (sqr a) (sqr b))
-					 (sqr c))))
+  "Are a, b, and c a pythagorean triple?"
+  (and (< a b c)
+       (is (+ (sqr a) (sqr b))
+           (sqr c))))
 
 (def num-solutions (p)
-	"How many right triangles with integer sides are there with
+  "How many right triangles with integer sides are there with
    this perimeter?"
-	(ret result 0
-		(upto a 1 p
-			(upto b a (- p a)
-				(let c (- p a b)
-					(when (triple a b c)
-						(++ result)))))))
+  (ret result 0
+    (upto a 1 p
+      (upto b a (- p a)
+        (let c (- p a b)
+          (when (triple a b c)
+            (++ result)))))))
 
 (def solve ()
-	"Solves PE problem 39."
-	(best #'> (range 1 1000) :key #'num-solutions))
+  "Solves PE problem 39."
+  (best #'> (range 1 1000) :key #'num-solutions))
