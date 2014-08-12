0f187e2e1bc1d2181d975555bf1e52bb62bdc26d
Michael <michaelmalis2@gmail.com>
2014-08-12 11:22:37 -0500

    Solved Problem 47

---
+++ pe047.lisp
@@ -0,0 +1,30 @@
+(in-package :clamp)
+
+;; https://github.com/smithzvk/cl-factoring
+(use-package :cl-factoring)
+
+(defparameter len-run* 4
+	"The length of the run needed.")
+
+(defparameter num-distinct* 4
+	"The number of distinct factors needed.")
+
+(def distinct-prime-factors (n k)
+	"Does n have k distinct prime factors?"
+	(is (len (dedup (factor n)))
+			k))
+
+(def run (start distinct length &optional (current 0))
+	"Starting at start, find the first number of the first run of 
+   consecutive numbers of length 'length'. All the numbers in which
+   have 'distinct' distinct prime factors."
+	(if (is length current)
+			  (- start current)
+			(distinct-prime-factors start distinct)
+			  (run (+ start 1) distinct length (+ current 1))
+			:else
+			  (run (+ start 1) distinct length 0)))
+
+(def solve ()
+	"Solves PE problem 47."
+	(run 2 num-distinct* len-run*))
