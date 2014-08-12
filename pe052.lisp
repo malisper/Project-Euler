56b7bcbbc2c90ecfd894cec5d8aaa37f9b0bc715
Michael <michaelmalis2@gmail.com>
2014-08-12 17:26:20 -0500

    Solved Problem 52

---
+++ pe052.lisp
@@ -0,0 +1,28 @@
+(in-package :clamp)
+
+(defparameter multiplier* 6
+	"The largest multiplier needed for this problem.")
+
+(def answer (n m)
+	"Is this the answer (do the numbers 1x 2x .. Mx this number, all use 
+   the same digits as this number)?"
+	(every [same-digits n (* n _)]
+				 (range 1 m)))
+
+(def digits (n)
+	"Returns a list of the digits of n."
+	(ret result '()
+		(while (> n 0)
+			(push (mod n 10) result)
+			(zap #'floor n 10))))
+
+(def same-digits (a b)
+	"Do these two numbers use the same digits?"
+	(iso (sort #'< (digits a))
+			 (sort #'< (digits b))))
+
+(def solve ()
+	"Solves PE problem 52."
+	(loop for i from 1
+			  if (answer i multiplier*)
+			    return i))
