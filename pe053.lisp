3569fa7a700a8c2f6a5d130d20d672a183026cdf
Michael <michaelmalis2@gmail.com>
2014-08-12 17:42:41 -0500

    Solved Problem 53

---
+++ pe053.lisp
@@ -0,0 +1,26 @@
+(in-package :clamp)
+
+(defparameter number* 1000000
+	"The number the other numbers have to be greater than for this
+   problem.")
+
+(defparameter largest-n* 100
+	"The upper bound on this problem for n.")
+
+(defmemo choose (n r)
+	"Returns nCr (n choose r)."
+	(if (< n r)
+			  0
+			(in r 0 n)
+			  1
+			:else
+			  (+ (choose (- n 1) r)
+					 (choose (- n 1) (- r 1)))))
+
+(def solve ()
+	"Solves PE problem 53."
+	(ret result 0
+		(upto n 0 largest-n*
+			(upto r 0 n
+				(when (> (choose n r) number*)
+					(++ result))))))
