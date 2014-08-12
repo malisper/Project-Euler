6e21e66c66ad2ebe99dc0f3d68b0cc5598089a50
Michael <michaelmalis2@gmail.com>
2014-08-12 18:22:56 -0500

    I think I fixed the indentation problem.

--- pe053.lisp
+++ pe053.lisp
@@ -1,29 +1,26 @@
 (in-package :clamp)
 
-;;;; NOTICE: If you are viewing this on GitHub, there appears to be
-;;;; some sort of indentation problem.
-
 (defparameter greater-than* 1000000
   "The number the other numbers have to be greater than for this
    problem.")
 
 (defparameter largest-n* 100
-	"The upper bound on this problem for n.")
+  "The upper bound on this problem for n.")
 
 (defmemo choose (n r)
-	"Returns nCr (n choose r)."
-	(if (< n r)
-			  0
-			(in r 0 n)
-			  1
-			:else
-			  (+ (choose (- n 1) r)
-					 (choose (- n 1) (- r 1)))))
+  "Returns nCr (n choose r)."
+  (if (< n r)
+        0
+      (in r 0 n)
+        1
+      :else
+        (+ (choose (- n 1) r)
+           (choose (- n 1) (- r 1)))))
 
 (def solve ()
-	"Solves PE problem 53."
-	(ret result 0
-		(upto n 0 largest-n*
-			(upto r 0 n
-				(when (> (choose n r) greater-than*)
-					(++ result))))))
+  "Solves PE problem 53."
+  (ret result 0
+    (upto n 0 largest-n*
+      (upto r 0 n
+        (when (> (choose n r) greater-than*)
+          (++ result))))))
