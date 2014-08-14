99b34a5034ffb8ebf66004173235240d3af4f786
Michael <michaelmalis2@gmail.com>
2014-08-14 17:43:36 -0500

    Solved Problem 22

---
+++ pe022.lisp
@@ -0,0 +1,28 @@
+(in-package :clamp)
+
+(defparameter file* "~/names.txt"
+  "The file containing the names for this problem.")
+
+(def parse-file ()
+  "Returns a list containing all of the names in the file, sorted
+   in alphabetical order."
+  (sort #'string<
+        (tokens (filechars file*)
+                (complement #'alpha-char-p))))
+
+(def charval (c)
+  "Returns the alphabetical value of a char (A=1, B=2, .., Z=26)."
+  (- (char-code c) #.(- (char-code #\A) 1)))
+
+(def alphaval (name)
+  "Returns the alphaval of this name. The name score is the sum of
+   the alphavals of the characters in the name."
+  (reduce #'+ name :key #'charval))
+
+(def solve ()
+  "Solves PE problem 22."
+  (loop for name in (parse-file)
+        for i from 1
+        sum (* i (alphaval name))))
+
+
