763206753d95cddbbb4d4d2aecfd91f0710ff9d8
Michael <michaelmalis2@gmail.com>
2014-08-14 22:55:53 -0500

    def -> defmemo

--- pe031.lisp
+++ pe031.lisp
@@ -6,7 +6,7 @@
 (defparameter coins* '(200 100 50 20 10 5 2 1)
   "The coins we can use to make change.")
 
-(def num-ways (n &optional (coins coins*))
+(defmemo num-ways (n &optional (coins coins*))
   "The number of ways we can make change for the argument."
   (if (or (< n 0) (null coins))
         0
