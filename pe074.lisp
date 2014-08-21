7b0786d6f17fd8c1ad4f8dbbdced9e39d329b562
Michael <michaelmalis2@gmail.com>
2014-08-21 11:01:02 -0500

    Completed Problem 74

---
+++ pe074.lisp
@@ -0,0 +1,37 @@
+(in-package :clamp)
+
+(defparameter largest* 1000000
+  "The upper bound on the problem.")
+
+(defparameter chain-length* 60
+  "The length of the chain needed.")
+
+(def fact (n)
+  "Returns the factorial of n."
+  (if (is n 0)
+      1
+      (* n (fact (- n 1)))))
+
+(def sum-fact-of-digits (n)
+  "Returns the sum of the factorial of the digits of n."
+  (if (is n 0)
+      0
+      (+ (fact (mod n 10)) (sum-fact-of-digits (floor n 10)))))
+
+(def valid (n)
+  "Does this number have a chain of the proper length?"
+  (with (current   n
+         num-times 0 ; This is needed in case of early breaking.
+         seen      (table)) 
+    (loop repeat chain-length*
+          while (no (gethash current seen))
+          do
+          (= (gethash current seen) t)
+          (zap #'sum-fact-of-digits current)
+          (++ num-times)
+          finally (return (and (is num-times chain-length*)
+                               (gethash current seen))))))
+
+(def solve ()
+  "Solves PE problem 74."
+  (loop for i from 1 below largest* count (valid i)))
