819edeb722edb3efd22d33f31a4c1798eb608ded
Michael <michaelmalis2@gmail.com>
2014-09-19 15:29:18 -0500

    Added some ssyntax.

--- pe020.lisp
+++ pe020.lisp
@@ -1,4 +1,4 @@
-(in-package :clamp)
+(in-package :experimental)
 
 (defparameter largest* 100
   "The upper bound on the problem")
@@ -7,14 +7,14 @@
   "Sums the digits of its argument."
   (if (is n 0)
       0
-      (+ (mod n 10) (sum-digits (floor n 10)))))
+      (+ (mod n 10) (sum-digits+floor n 10))))
 
 (def fact (n)
   "Calculates the factorial of its argument."
   (if (is n 0)
       1
-      (* n (fact (- n 1)))))
+      (* n (fact+dec n))))
 
 (def solve ()
   "Solves PE problem 20"
-  (sum-digits (fact largest*)))
+  (sum-digits+fact largest*))
